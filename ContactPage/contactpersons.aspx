1b1147c7ba50c1c34ff738683eea9d8d4964681c
Roshan Louhar <rolouhar@microsoft.com>
2018-06-19 20:49:49 +0530

    Contact Page Added
    Contact Page Added for web app and changing sms authority code.

---
+++ ContactPage/contactpersons.aspx
@@ -0,0 +1,161 @@
+﻿<%@ Page Title="Contact Person" Language="C#" MasterPageFile="~/ac.master" AutoEventWireup="true" CodeFile="contactpersons.aspx.cs" Inherits="contactpersons" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="cc" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+
+
+    <span id="s" runat="server"></span>
+
+    <div>
+        <span id="spnMsg" runat="server" style="color: red; font-weight: bold" visible="false"></span>
+    </div>
+
+    <asp:GridView ID="gvContactPerson" runat="server" AutoGenerateColumns="false" PageSize="50" DataKeyNames="deviceid , cpid"
+        AllowPaging="true" CssClass="table table-striped table-bordered  textcenter" OnRowDeleting="gvContactPerson_RowDeleting" OnRowDataBound="gvContactPerson_RowDataBound">
+        <Columns>
+            <%-- <asp:TemplateField>
+                    <ItemTemplate>
+                        <asp:CheckBox ID="chkGroupCotrol" runat="server" />
+                    </ItemTemplate>
+                    <HeaderStyle HorizontalAlign="Center" />
+                </asp:TemplateField>--%>
+            <asp:TemplateField HeaderText="SR No.">
+                <ItemTemplate>
+                    <asp:Label ID="lblSN" runat="server" Text='<%#Container.DataItemIndex+1 %>'></asp:Label>
+                </ItemTemplate>
+                <HeaderStyle HorizontalAlign="Center" />
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Name">
+                <ItemTemplate>
+                    <asp:Label ID="lblName" runat="server" Text='<%# Eval("name") %>'></asp:Label>
+                </ItemTemplate>
+                <HeaderStyle HorizontalAlign="Center" />
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Mobile No">
+                <ItemTemplate>
+                    <asp:Label ID="lblMno" runat="server" Text='<%# Eval("mobile") %>'></asp:Label>
+                </ItemTemplate>
+                <HeaderStyle HorizontalAlign="Center" />
+            </asp:TemplateField>
+            <asp:TemplateField HeaderText="Address">
+                <ItemTemplate>
+                    <asp:Label ID="lblAddress" runat="server" Text='<%# Eval("address") %>'></asp:Label>
+                </ItemTemplate>
+                <HeaderStyle HorizontalAlign="Center" />
+            </asp:TemplateField>
+            <asp:TemplateField HeaderStyle-HorizontalAlign="Left" HeaderText="Option">
+                <ItemTemplate>
+                    <asp:LinkButton ID="lnkDelete" runat="server" Text="Delete" CommandName="Delete" CausesValidation="false" OnClientClick="return confirm('Are you sure you want to delete this contact person?');"></asp:LinkButton>
+                </ItemTemplate>
+            </asp:TemplateField>
+        </Columns>
+        <PagerStyle HorizontalAlign="Right" CssClass="pagination-ys" />
+    </asp:GridView>
+
+    <div>
+        <span id="spnSMSSend" runat="server" style="color: green; font-weight: bold" visible="false"></span>
+    </div>
+    <div class="row">
+        <div class=" col-md-12 col-lg-12 ">
+            <table class="table table-user-information">
+                <tbody>
+                    <tr>
+                        <td>Device ID :</td>
+                        <td>
+                            <asp:TextBox ID="txtDeviceId" CssClass="form-control" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Zone - Ward :</td>
+                        <td>
+                            <asp:TextBox ID="txtZone" CssClass="form-control" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Area & Location :</td>
+                        <td>
+                            <asp:TextBox ID="txtAreaLocation" CssClass="form-control" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Status : </td>
+                        <td>
+                            <asp:TextBox ID="txtStatus" CssClass="form-control" runat="server"></asp:TextBox>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Mobile No :</td>
+                        <td>
+                            <asp:TextBox ID="txtSendMobile" CssClass="form-control" runat="server"></asp:TextBox>
+                            <asp:RequiredFieldValidator ID="RFVtxtSendMobile" runat="server" ControlToValidate="txtSendMobile" Display="Dynamic" ErrorMessage="Please enter the mobile no" ValidationGroup="m1"></asp:RequiredFieldValidator>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td>
+                            <asp:Button ID="btnSendSMS" runat="server" Text="Send SMS Alert" CssClass="btn-danger" ValidationGroup="m1" OnClick="btnSendSMS_Click" />
+                        </td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h3 class="panel-title" id="h3Name" runat="server">Add New Contact Person</h3>
+        </div>
+        <div class="panel-body">
+            <div class="row">
+                <div class=" col-md-12 col-lg-12 ">
+                    <table class="table table-user-information">
+                        <tbody>
+                            <tr>
+                                <td colspan="3">
+                                    <b><span id="spnMessage" runat="server"></span></b>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Name :</td>
+                                <td>
+                                    <asp:TextBox ID="txtName" CssClass="form-control" runat="server"></asp:TextBox></td>
+                                <td>
+                                    <asp:RequiredFieldValidator ID="RFVtxtName" runat="server" ControlToValidate="txtName" ErrorMessage="Please Enter Name" ValidationGroup="cp"></asp:RequiredFieldValidator>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Mobile :</td>
+                                <td>
+                                    <asp:TextBox ID="txtMobile" CssClass="form-control" runat="server"></asp:TextBox>
+                                    <cc:FilteredTextBoxExtender ID="FTBtxtMobile" runat="server" TargetControlID="txtMobile" FilterMode="ValidChars" ValidChars="0123456789+"></cc:FilteredTextBoxExtender>
+                                </td>
+                                <td>
+                                    <asp:RequiredFieldValidator ID="RFVtxtMobile" runat="server" ControlToValidate="txtMobile" ErrorMessage="Please Enter Mobile" ValidationGroup="cp"></asp:RequiredFieldValidator>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>Address :</td>
+                                <td>
+                                    <asp:TextBox ID="txtAddress" CssClass="form-control" runat="server"></asp:TextBox></td>
+                                <td>
+                                    <asp:RequiredFieldValidator ID="RFVtxtAddress" runat="server" ControlToValidate="txtAddress" ErrorMessage="Please Enter Address" ValidationGroup="cp"></asp:RequiredFieldValidator>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>
+                                    <asp:Button ID="btnAddNewAddress" CssClass="btn btn-primary" runat="server" Text="Submit" ValidationGroup="cp" OnClick="btnAddNewAddress_Click" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
+
---
+++ ContactPage/contactpersons.aspx.cs
@@ -0,0 +1,323 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+public partial class contactpersons : System.Web.UI.Page
+{
+    common Ocommon = new common();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (Session["userid"] == null)
+        {
+            Response.Redirect(Page.ResolveUrl("~/login.aspx"));
+        }
+        if (!Page.IsPostBack)
+        {
+            if (Request.QueryString["deviceid"] != null)
+            {
+                Bind_Contact_Person_Information(Convert.ToInt64(Ocommon.Decrypt(Request.QueryString["deviceid"].ToString(), true)));
+                Bind_SMS_Alert_Data(Convert.ToInt64(Ocommon.Decrypt(Request.QueryString["deviceid"].ToString(), true)));
+            }
+            HtmlGenericControl hPageTitle = (HtmlGenericControl)this.Page.Master.FindControl("hPageTitle");
+            hPageTitle.InnerText = "Contact Person";
+        }
+
+    }
+
+    private void Bind_SMS_Alert_Data(Int64 deviceID)
+    {
+        txtDeviceId.Text = Ocommon.Decrypt(Request.QueryString["deviceid"].ToString(), true);
+        txtZone.Text = Ocommon.Decrypt(Request.QueryString["zone"].ToString(), true);
+        txtAreaLocation.Text = Ocommon.Decrypt(Request.QueryString["area"].ToString(), true);
+        txtStatus.Text = Ocommon.Decrypt(Request.QueryString["status"].ToString(), true);
+
+        MySqlConnection con = new MySqlConnection("Server=115.124.109.110;Port=3306;Database=slc;Uid=slcuser;Pwd=Sc@12;Convert Zero Datetime=True;");
+        DataTable dt1 = new DataTable();
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand("Select mobile from cp where deviceid=" + deviceID + " order by mobile LIMIT 1");
+        MySqlDataAdapter sda = new MySqlDataAdapter();
+        cmd.Connection = con;
+        sda.SelectCommand = cmd;
+        sda.Fill(dt1);
+        if (dt1 != null)
+        {
+            if (dt1.Rows.Count > 0)
+            {
+                txtSendMobile.Text = dt1.Rows[0]["mobile"].ToString();
+            }
+        }
+        con.Close();
+    }
+
+    private void Bind_Contact_Person_Information(Int64 deviceID)
+    {
+        MySqlConnection con = new MySqlConnection("Server=115.124.109.110;Port=3306;Database=slc;Uid=slcuser;Pwd=Sc@12;Convert Zero Datetime=True;");
+        DataTable dt1 = new DataTable();
+
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand("Select * from cp where deviceid=" + deviceID);
+        MySqlDataAdapter sda = new MySqlDataAdapter();
+        cmd.Connection = con;
+        sda.SelectCommand = cmd;
+        sda.Fill(dt1);
+        if (dt1 != null)
+        {
+            if (dt1.Rows.Count > 0)
+            {
+                gvContactPerson.DataSource = dt1;
+                gvContactPerson.DataBind();
+            }
+        }
+        con.Close();
+    }
+
+    protected void gvContactPerson_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            if ((e.Row.RowState == DataControlRowState.Normal) || (e.Row.RowState == DataControlRowState.Alternate))
+            { }
+        }
+    }
+
+    protected void btnAddNewAddress_Click(object sender, EventArgs e)
+    {
+        MySqlConnection con = new MySqlConnection("Server=115.124.109.110;Port=3306;Database=slc;Uid=slcuser;Pwd=Sc@12;Convert Zero Datetime=True;");
+        DataTable dt1 = new DataTable();
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand("Select * from cp where deviceid=" + Convert.ToInt64(Convert.ToInt64(Ocommon.Decrypt(Request.QueryString["deviceid"].ToString(), true))) + "  and mobile=" + txtMobile.Text.Trim());
+        MySqlDataAdapter sda = new MySqlDataAdapter();
+        cmd.Connection = con;
+        sda.SelectCommand = cmd;
+        sda.Fill(dt1);
+        if (dt1 != null)
+        {
+            if (dt1.Rows.Count > 0)
+            {
+                spnMessage.Visible = true;
+                spnMessage.Style.Add("color", "red");
+                spnMessage.InnerHtml = "Mobile Number already exists!";
+            }
+            else
+            {
+                Insert_Phone();
+            }
+        }
+        else
+        {
+            Insert_Phone();
+        }
+    }
+
+    public void Insert_Phone()
+    {
+        MySqlConnection con = new MySqlConnection("Server=115.124.109.110;Port=3306;Database=slc;Uid=slcuser;Pwd=Sc@12;Convert Zero Datetime=True;");
+        con.Close();
+        string query = "INSERT into slc.cp (deviceid,name,address,mobile) values (" + Convert.ToInt64(Convert.ToInt64(Ocommon.Decrypt(Request.QueryString["deviceid"].ToString(), true))) + ",'" + txtName.Text.Trim() + "','" + txtAddress.Text.Trim() + "','" + txtMobile.Text.Trim() + "');";
+        MySqlCommand cmd = new MySqlCommand(query, con);
+        con.Open();
+        spnMsg.Visible = false;
+        spnMessage.Visible = true;
+        spnMessage.Style.Add("color", "green");
+        try
+        {
+            cmd.ExecuteNonQuery();
+            spnMessage.InnerHtml = "Contact Person Added Successfully!";
+            clearAll();
+            Bind_Contact_Person_Information(Convert.ToInt64(Convert.ToInt64(Ocommon.Decrypt(Request.QueryString["deviceid"].ToString(), true))));
+        }
+        catch (Exception ex)
+        {
+            clearAll();
+            spnMessage.InnerHtml = ex.Message;
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    public void clearAll()
+    {
+        txtName.Text = string.Empty;
+        txtMobile.Text = string.Empty;
+        txtAddress.Text = string.Empty;
+    }
+
+    protected void gvContactPerson_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        Int32 cpid = Convert.ToInt32(gvContactPerson.DataKeys[e.RowIndex]["cpid"]);
+        Int32 deviceid = Convert.ToInt32(gvContactPerson.DataKeys[e.RowIndex]["deviceid"]);
+
+        MySqlConnection con = new MySqlConnection("Server=115.124.109.110;Port=3306;Database=slc;Uid=slcuser;Pwd=Sc@12;Convert Zero Datetime=True;");
+        con.Close();
+        string query = "Delete From cp where cpid=" + Convert.ToInt32(gvContactPerson.DataKeys[e.RowIndex]["cpid"]) + " and deviceid=" + Convert.ToInt32(gvContactPerson.DataKeys[e.RowIndex]["deviceid"]) + " ";
+        MySqlCommand cmd = new MySqlCommand(query, con);
+        con.Open();
+        spnMessage.Visible = false;
+        spnMsg.Visible = true;
+        spnMsg.Style.Add("color", "green");
+        try
+        {
+            cmd.ExecuteNonQuery();
+            spnMsg.InnerHtml = "Contact Person Deleted Successfully!";
+            Bind_Contact_Person_Information(Convert.ToInt64(Ocommon.Decrypt(Request.QueryString["deviceid"].ToString(), true)));
+        }
+        catch (Exception ex)
+        {
+            clearAll();
+            spnMsg.InnerHtml = ex.Message;
+        }
+        finally
+        {
+            con.Close();
+        }
+    }
+
+    //protected void btnSendSMS_Click(object sender, EventArgs e)
+    //{
+    //    //string Msg = "Device id :6025  zone 1 ward 1,  Address :Arilova Last Bus Sto-SS-88ABCSivaji Nagar Status: Power Failure";
+
+    //    //Status List  -  Over Load , Over Voltage , Under Load , Over Frequency , Under Frequency , Over Temperature, 
+
+    //    string Status = txtStatus.Text.Trim().ToLower();
+    //    spnSMSSend.Visible = true;
+
+    //    if (Session["temp-city"].ToString() == "1")
+    //    {
+    //        if ((Status == "Power Failure".ToLower() || Status == "Under Voltage".ToLower()))
+    //        {
+    //            string Msg = "Device id :" + txtDeviceId.Text.Trim() + "   Address :" + txtAreaLocation.Text.Trim() + "   Status :" + txtStatus.Text.Trim() + "   Zone :" + txtZone.Text.Trim();
+    //            string OPTINS = "ESMART";
+    //            string password = "esmart";
+    //            string MobileNumber = txtSendMobile.Text.Trim();  //"9021191362"; 
+    //            string strUrl = "http://bulksmsgateway.in/sendmessage.php?user=esmart&password=" + password + "&message=" + Msg + "&sender=" + OPTINS + "&mobile=" + MobileNumber + "&type=" + 3;
+    //            System.Net.WebRequest request = System.Net.WebRequest.Create(strUrl);
+    //            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+    //            Stream s = (Stream)response.GetResponseStream();
+    //            StreamReader readStream = new StreamReader(s);
+    //            string dataString = readStream.ReadToEnd();
+    //            response.Close();
+    //            s.Close();
+    //            readStream.Close();
+    //            spnSMSSend.InnerText = "SMS Send Successfully";
+    //        }
+    //        else
+    //        {
+    //            spnSMSSend.InnerText = "Sorry you have no authority send message.";
+    //        }
+    //    }
+    //    else
+    //    {
+    //        spnSMSSend.InnerText = "Sorry you have no authority send message.";
+    //    }
+    //}
+
+    protected void btnSendSMS_Click(object sender, EventArgs e)
+    {
+        string Status = txtStatus.Text.Trim().ToLower();
+        spnSMSSend.Visible = true;
+        bool send = StatusList(txtStatus.Text.Trim().ToLower());
+        if (send)
+        {
+            string Msg = "Device Id - " + txtDeviceId.Text.Trim() + "  Address - " + txtAreaLocation.Text.Trim() + "   Status - " + txtStatus.Text.Trim() + "   Zone - " + txtZone.Text.Trim();
+            string OPTINS = "SLCSMS";
+            string username = "29prash";
+            string password = "nn1234";
+            string baseURL = "http://quicksms.in/sendsms.asp";           
+            string MobileNumber = txtSendMobile.Text.Trim();  //"7588518728";	
+
+            string strUrl = baseURL + "?user=" + username + "&password=" + password + "&PhoneNumber=" + MobileNumber + "&sender=" + OPTINS + "&Text=" + System.Web.HttpUtility.UrlEncode(Msg) + "";
+
+            System.Net.WebRequest request = System.Net.WebRequest.Create(strUrl);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream s = (Stream)response.GetResponseStream();
+            StreamReader readStream = new StreamReader(s);
+            string dataString = readStream.ReadToEnd();
+            response.Close();
+            s.Close();
+            readStream.Close();
+            spnSMSSend.InnerText = "SMS Send Successfully";
+        }
+        else
+        {
+            spnSMSSend.InnerText = "Sorry you have no authority send message.";
+        }
+    }
+
+    private bool StatusList(string status)
+    {
+        bool send = false;
+        switch (status.Trim().ToLower())
+        {
+            case "Over Voltage":
+                send = true;
+                break;
+            case "Under Voltage":
+                send = true;
+                break;
+            case "Over Load":
+                send = true;
+                break;
+            case "Power Failure":
+                send = true;
+                break;
+            case "Group Failure":
+                send = true;
+                break;
+            case "Power Down":
+                send = true;
+                break;
+            case "Power Up":
+                send = true;
+                break;
+            case "Power Fail":
+                send = true;
+                break;
+            case "NV Fault":
+                send = true;
+                break;
+            case "R I/P MCB Trip":
+                send = true;
+                break;
+            case "Y I/P MCB Trip":
+                send = true;
+                break;
+            case "B I/P MCB Trip":
+                send = true;
+                break;
+            case "R O/P MCP Trip":
+                send = true;
+                break;
+            case "Y O/P MCP Trip":
+                send = true;
+                break;
+            case "B O/P MCP Trip":
+                send = true;
+                break;
+            case "Contactor Failure":
+                send = true;
+                break;
+            case "Panel Door Open":
+                send = true;
+                break;
+            case "IF Fault":
+                send = true;
+                break;
+            default:
+                send = true;
+                break;
+        }
+
+        return send;
+    }
+}
